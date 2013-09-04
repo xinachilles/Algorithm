310b00cffc26eb82e305e91c8d0a4bd367b074dd
xinachilles <huyoucai.huxin@gmail.com>
2013-09-03 22:57:12 -0600

    9.9

--- ClassLibrary/Recursion.cs
+++ ClassLibrary/Recursion.cs
@@ -384,10 +384,10 @@ namespace ClassLibrary
         public int makeChange(int n, int denom)
         {
             int next_denom = 0;
-            
+
             switch (denom)
             {
-               case 25:
+                case 25:
                     next_denom = 10;
                     break;
                 case 10:
@@ -404,7 +404,7 @@ namespace ClassLibrary
             for (int i = 0; i * denom <= n; i++)
             {
                 ways += makeChange(n - i * denom, next_denom);
-                Console.WriteLine(denom.ToString()); 
+                Console.WriteLine(denom.ToString());
 
             }
             return ways;
@@ -412,8 +412,70 @@ namespace ClassLibrary
 
 
         #endregion
+
+        #region 9.9
+
+        //  Write an algorithm to prim all ways of arranging eight queens on an 8x8 chess
+        //board so that none of them share the same row, column or diagonal. In this case,
+        //"diagonal" means all diagonals, not just the two that bisect the board.
+
+
+        int GRID_SIZE = 8;
+
+        public void placeQueens(int row, int[] columns, ArrayList results)
+        {
+            if (row == GRID_SIZE)
+            { // Found valid placement
+                results.Add(columns.Clone());
+            }
+            else
+            {
+                for (int col = 0; col < GRID_SIZE; col++)
+                {
+                    if (checkValid(columns, row, col))
+                    {
+                        columns[row] = col; // Place queen
+                        placeQueens(row + 1, columns, results);
+                    }
+                }
+            }
+        }
+
+        /* Check if (rowl, columnl) is a valid spot for a queen by checking
+        * if there is a queen in the same column or diagonal. We don't
+        * need to check it for queens in the same row because the calling
+        * placeQueen only attempts to place one queen at a time. We know
+        * this row is empty. */
+        private bool checkValid(int[] columns, int row1, int column1)
+        {
+            for (int row2 = 0; row2 < row1; row2++)
+            {
+                int column2 = columns[row2];
+                /* Check if (row2, column2) invalidates (rowl, columnl) as a queen spot. */
+
+                /* Check if rows have a queen in the same column */
+                if (column1 == column2)
+                {
+                    return false;
+                }  
+
+                /* Check diagonals: if the distance between the columns equals the distance between the rows, then they're in the
+                same diagonal. */
+                int columnDistance = Math.Abs(column2 - column1);
+
+                /* rowl > row2, so no need for abs */
+                int rowDistance = row1 - row2;
+                if (columnDistance == rowDistance)
+                {
+                    return false;
+                } // end if 
+            }// end for
+            return true;
+
+        }
+
+        #endregion
+
     }
 }
-
-
 
